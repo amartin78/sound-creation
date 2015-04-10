27e844507a911be5d562404f8a8c179720809432
Antonio <ammj24@hotmail.com>
2015-04-10 05:21:44 +0100

    Mail from Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Enable ActionMailer to use Sendgrid plugin to send emails from Heroku
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp.sendgrid.net',
+	:port => '587',
+	:authentication => :plain,
+	:username => ENV['SENGRID_USERNAME'],
+	:password => ENV['SENGRID_PASSWORD'],
+	:domain => 'heroku.com',
+	:enable_starttls_auto => true
+}
