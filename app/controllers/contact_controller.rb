801eb177170bef4771dd5d6d5a77cbc15d69b51b
Antonio <ammj24@hotmail.com>
2015-04-10 06:07:35 +0100

    flash message

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -7,7 +7,7 @@ class ContactController < ApplicationController
   def form_out
   	@mail = ActionMailer::Base.mail(:from => params[:email], :to => "ammj24@hotmail.com", :subject => params[:subject], 
   					:body => params[:message]).deliver
-  	flash[:notice] = "Message on transit..."
+  	flash[:notice] = "Thanks for your submission. I will contact you as soon as possible."
   	redirect_to :back
   end
 
