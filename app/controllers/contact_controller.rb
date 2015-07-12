8aa37c9f95716411735a0ac17a2a99f9ec234f6c
Antonio <ammj24@hotmail.com>
2015-07-12 18:46:07 +0100

    email-params

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,7 +6,7 @@ class ContactController < ApplicationController
 
   def form_out
   	@mail = ActionMailer::Base.mail(:from => params[:email], :to => "ammj24@hotmail.com", :subject => params[:subject], 
-  					:body => params[:message]).deliver
+  					:body => params[:message], :name => params[:name], :phone => params[:phone]).deliver
   	flash[:notice] = "Thanks for your submission. I will contact you as soon as possible."
   	redirect_to :back
   end
